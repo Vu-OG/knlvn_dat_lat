1b7e0c057f72a418fa749631239a13e5cb9425d5
takiindev <takin2k5@gmail.com>
2025-03-11 22:37:40 +0700

    up

--- src/js/header.js
+++ src/js/header.js
@@ -3,22 +3,11 @@ header.innerHTML = `
             <img src="../src/img/logo.png" alt="">
         </div>
         <ul class="nav">
-            <li onclick = "domMaincontent(this)" class="trang-chu"><a>Trang chủ</a></li>
-            <li onclick = "domMaincontent(this)" class="kham-pha"><a>Khám phá</a></li>
-            <li onclick = "domMaincontent(this)" class="ve-chung-toi"><a>Về chúng tôi</a></li>
-            <li onclick = "domMaincontent(this)" class="album-anh"><a>Album ảnh</a></li>
+            <li class="trang-chu"><a href = "#banner" >Trang chủ</a></li>
+            <li class="kham-pha"><a href = "#section1">Khám phá</a></li>
+            <li class="ve-chung-toi"><a href = "#section2">Về chúng tôi</a></li>
+            <li class="album-anh"><a>Album ảnh</a></li>
         </ul>
     `
 
 head.innerHTML += "<link rel='stylesheet' href='./src/css/header.css'>";
-
-let namePage = localStorage.getItem('namePage');
-if(namePage == null || namePage == ''){
-    namePage = 'trang-chu';
-}
-var navLink = document.querySelectorAll('.nav li');
-navLink.forEach((li) => {
-    if (li.classList.contains(namePage)) {
-        li.classList.add('active');
-    }
-});
